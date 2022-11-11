b7fcfcbdd42b73ab5a04762c3ea9fb5ec6f2d0c2
Fabio Teichmann <ft@blocksize-capital.com>
2022-11-11 15:22:57 +0200

    refactoring

--- main.go
+++ main.go
@@ -4,19 +4,17 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/ele-fant/microservices/handlers"
 )
 
 func main() {
 	// register a function to DefaulServeMux
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 
-	})
-
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye World!")
-	})
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 	// addr = bind (here to all IP-addesses with port 9090)
 	// handler = if nil -> uses DefaulServeMux
 	http.ListenAndServe(":9090", nil)
