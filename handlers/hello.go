c8867a9c777f19e1b36c6b4d6ae3511a848b2e4d
Fabio Teichmann <ft@blocksize-capital.com>
2022-11-11 16:24:23 +0200

    refactoring finished; added server

--- handlers/hello.go
+++ handlers/hello.go
@@ -2,7 +2,7 @@ package handlers
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 )
@@ -17,7 +17,7 @@ func NewHello(l *log.Logger) *Hello {
 
 func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	h.l.Println("Hello World!")
-	d, err := ioutil.ReadAll(r.Body)
+	d, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(rw, "Oooops", http.StatusBadRequest)
 		/* MANUAL VERSION
--- main.go
+++ main.go
@@ -1,21 +1,55 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
-	"github.com/ele-fant/microservices/handlers"
+	"github.com/ele-fant/handlers"
 )
 
 func main() {
 	// register a function to DefaulServeMux
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	bh := handlers.NewBye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", bh)
+
+	// create a server
+	s := &http.Server{
+		Addr:    ":9090",
+		Handler: sm,
+		// control timeouts
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
 	// addr = bind (here to all IP-addesses with port 9090)
 	// handler = if nil -> uses DefaulServeMux
-	http.ListenAndServe(":9090", nil)
+	//http.ListenAndServe(":9090", sm)
+
+	timeOutContext, _ := context.WithTimeout(context.Background(), 30*time.Second)
+
+	s.Shutdown(timeOutContext)
 }
