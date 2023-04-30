fcc4b333891c5fb31bd7b8dac455e42772ff0895
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-04-30 22:11:33 +0300

    refactoring handlers into module

--- main.go
+++ main.go
@@ -1,37 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
+	"microservice/handlers"
 	"net/http"
+	"os"
 )
 
 func main() {
+	logger := log.New(os.Stdout, "product-api", log.LstdFlags)
+	handler := handlers.NewHello(logger)
+	gbHandler := handlers.NewGoodbye(logger)
+
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/", handler)
+	serveMux.Handle("/goodbye", gbHandler)
 	// HandleFunc is a convenience function that registers a function on a path called
 	// "Default ServMux" (=http handler / http request multiplexer)
 	// The ServMux determines which function gets activated.
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		// read slices of data from the http request's body
-		data, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			// inform sender on error
-			http.Error(rw, "Oops", http.StatusBadRequest)
-			return
-			// log.Println(err)
-		}
+	// http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 
-		// log.Printf("Data: %s\n", data)
-		// print back to the response writer
-		fmt.Fprintf(rw, "Hello %s", data)
-	})
+	// })
 
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Good bye World")
-	})
+	// http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+	// 	log.Println("Good bye World")
+	// })
 
 	// BIND address, HANDLER
 	// most basic webserver; if HANDLER not specified, it uses the default ServMux
-	http.ListenAndServe(":9090", nil)
+	http.ListenAndServe(":9090", serveMux)
 }
