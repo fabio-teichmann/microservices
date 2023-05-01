865273e465ad162dd66ac5cd8b47055ea057e633
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-05-01 10:22:50 +0300

    added handler functinality; refactored JSON encoding logic into GET function

---
+++ handlers/products.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"log"
+	"microservice/data"
+	"net/http"
+)
+
+type Products struct {
+	logger *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	listProducts := data.GetProducts()
+	// encoding JSON using marshal -> allocates data to memory
+	// d, err := json.Marshal(listProducts) DEPRECATED
+	err := listProducts.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "unable to marshal JSON", http.StatusInternalServerError)
+	}
+	// write marshalled data
+	// rw.Write(d) DEPRECATED
+}
