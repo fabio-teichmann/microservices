f14d30c10df09dac3cb06d1d35cb556eb43c5db0
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-05-01 10:29:40 +0300

    implemented GET serve http for product handler

--- handlers/products.go
+++ handlers/products.go
@@ -15,7 +15,15 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
 
+	// handle UPDATE
+
+	// catch all other cases
+	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
