2d76e8330f38d78574d46af8570b99162c481d11
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-05-04 13:57:40 +0300

    refactored documentation related structs into its own file

---
+++ handlers/docs.go
@@ -0,0 +1,38 @@
+// Package classification of Product API
+//
+// Documentation for Product API
+//
+//	Schemes: http
+//	BasePath: /
+//	Version: 1.0.0
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//
+// swagger:meta
+package handlers
+
+import "microservice/data"
+
+// A list of products returned in the response
+// swagger:response productsResponse
+type productsResponse struct {
+	// All products in the system
+	// in: body
+	Body []data.Product
+}
+
+// swagger:response noContent
+type productsNoContent struct {
+}
+
+// swagger:parameters DeleteProduct
+type productIDParameterWrapper struct {
+	// The id of the product to delete from the database
+	// in: path
+	// required: true
+	ID int `json:"id"`
+}
