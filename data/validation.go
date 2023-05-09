abcf45a409e3b27955e43302e374ed62ad743727
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-05-09 10:09:44 +0300

    extracted and extended the validation logic into its own file

---
+++ data/validation.go
@@ -0,0 +1,76 @@
+package data
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/go-playground/validator"
+)
+
+// ValidationError wraps the validators FieldError so we do not
+// expose this to our code
+type ValidationError struct {
+	validator.FieldError
+}
+
+// ValidationErrors is a collection of ValidationError
+type ValidationErrors []ValidationError
+
+func (v ValidationError) Error() string {
+	return fmt.Sprintf(
+		"Key: '%s' Error: Field validation for '%s' failed on the '%s' tag",
+		v.Namespace(),
+		v.Field(),
+		v.Tag(),
+	)
+}
+
+func (v ValidationErrors) Errors() []string {
+	errs := []string{}
+	for _, err := range v {
+		errs = append(errs, err.Error())
+	}
+	return errs
+}
+
+// Validation contains
+type Validation struct {
+	validate *validator.Validate
+}
+
+func NewValidator() *Validation {
+	validate := validator.New()
+	// register custom validation function on specific tags
+	// in this case for sku
+	validate.RegisterValidation("sku", validateSKU)
+
+	return &Validation{validate}
+}
+
+func validateSKU(fl validator.FieldLevel) bool {
+	// sku is of format abc-abcd-abcde
+	re := regexp.MustCompile(`[a-z]+-[a-z]+-[a-z]+`)
+	matches := re.FindAllString(fl.Field().String(), -1)
+
+	if len(matches) != 1 {
+		return false
+	}
+
+	return true
+}
+
+func (v *Validation) Validate(i interface{}) ValidationErrors {
+	errs := v.validate.Struct(i).(validator.ValidationErrors)
+
+	if len(errs) == 0 {
+		return nil
+	}
+
+	var returnErrs ValidationErrors
+	for _, err := range errs {
+		// cast the FieldError into our ValidationError and append to the slice
+		ve := ValidationError{err.(validator.FieldError)}
+		returnErrs = append(returnErrs, ve)
+	}
+	return returnErrs
+}
